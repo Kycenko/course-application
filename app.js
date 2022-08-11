8d2ec8101e285805e3b18c988dd3b306ae3c9fb0
Vladislav Lagunovich <lagunovich.vladislav2003@gmail.com>
2022-08-11 20:16:40 +0300

    third commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-import { technologies } from './technologies';
+const technologies = [
+	{ title: 'HTML', description: '', type: 'html', done: true },
+	{ title: 'CSS', description: '', type: 'css', done: true },
+	{ title: 'JavaScript', description: '', type: 'js', done: false },
+	{ title: 'Git', description: '', type: 'git', done: false },
+	{ title: 'React', description: '', type: 'react', done: false },
+];
 
 const modal = document.querySelector('#modal');
 const content = document.querySelector('#content');
 const backdrop = document.querySelector('#backdrop');
+const progress = document.querySelector('#progress');
 
 content.addEventListener('click', openModal);
 
@@ -16,11 +23,52 @@ function closeModal() {
 	modal.classList.remove('open');
 }
 
-let html = '';
-
-for (let i = 0; i < technologies.length; i++) {
-	const tech = technologies[i];
-
-	html += ``;
+function init() {
+	renderCards();
+	renderProgress();
 }
-content.innerHTML = html;
+
+function renderCards() {
+	if (technologies.length === 0) {
+		content.innerHTML =
+			'<p class="empty">Технологий пока нет. Добавьте первую</p>';
+	} else {
+		content.innerHTML = technologies.map(toCard).join('');
+	}
+}
+
+function renderProgress() {
+	const percent = computeProgressPercent();
+	let background;
+	if (percent <= 30) {
+		background = '#e75a5a';
+	} else if (percent > 30 && percent < 70) {
+		background = '#f99415';
+	} else {
+		background = '#73ba3c';
+	}
+
+	progress.style.background = background;
+	progress.style.width = percent + '%';
+	progress.textContent = percent ? percent + '%' : '';
+}
+
+function computeProgressPercent() {
+	if (technologies.length === 0) return 0;
+
+	let doneCount = 0;
+	for (let i = 0; i < technologies.length; i++) {
+		if (technologies[i].done) doneCount++;
+	}
+
+	return Math.round((100 * doneCount) / technologies.length);
+}
+
+function toCard(tech) {
+	const doneClass = tech.done ? 'done' : '';
+	return `<div class="card ${doneClass}">
+					<h3>${tech.title}</h3>
+				</div>`;
+}
+
+init();
